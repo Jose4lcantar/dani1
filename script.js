58955c43618a197a44f62ea8e2be57b5243fc483
Daniel18UvH <ulisesvh.ti22@utsjr.edu.mx>
2024-02-08 19:09:34 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Puedes agregar funciones adicionales aquí según sea necesario
+    console.log("El DOM ha cargado correctamente.");
+});
