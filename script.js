febc8d35222941efd52f1a57244c2f5b4dd3658b
Daniel18UvH <ulisesvh.ti22@utsjr.edu.mx>
2024-02-09 08:33:31 -0600

    loginrender

--- script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Puedes agregar funciones adicionales aquí según sea necesario
-    console.log("El DOM ha cargado correctamente.");
-});
